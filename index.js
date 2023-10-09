4d4667db4c49ab214b8dda5c6734a14de75d9fc2
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2023-10-09 17:24:49 -0400

    Create "topMovies" array with data
    Created an array of objects (each obj a top 10 movie)
    Included info: title, director, year

--- index.js
+++ index.js
@@ -3,3 +3,57 @@ const express = require('express'),
 fs = require('fs'),
 path = require('path');
 const app = express();
+
+let topMovies = [
+    {
+    title: 'The Shawshank Redemption',
+    director: 'Frank Darabont',
+    year: '1994'
+    },
+    {
+    title: 'The Godfather',
+    author: 'Francis Ford Coppola',
+    year: '1972'
+    },
+    {
+    title: 'The Dark Knight',
+    author: 'Christopher Nolan',
+    year: '2008'
+    },
+    {
+    title:'The Godfather: Part II',
+    author: 'Francis Ford Coppola',
+    year: '1974'
+    },
+    {
+    title: '12 Angry Men',
+    author: 'Sidney Lumet',
+    year: '1957'
+    },
+    {
+    title: 'Schindler\'s List',
+    author: 'Steven Spielberg',
+    year: '1993'
+    },
+    {
+    title: 'The Lord of the Rings: The Return of the King',
+    author: 'Peter Jackson',
+    year: '2003'
+    },
+    {
+    title: 'Pulp Fiction',
+    author: 'Quentin Tarantino',
+    year: '1994'
+    },
+    {
+    title: 'The Lord of the Rings: The Fellowship of the Ring',
+    author: 'Peter Jackson',
+    year: '2001'
+    },
+    {
+    title: 'The Good, the Bad and the Ugly',
+    author: 'Sergio Leone',
+    year: '1966'
+    },
+];
+
