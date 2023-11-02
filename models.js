2b2ce11dc964c1cefd6dc9125ecd6a65312c3b5f
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2023-11-02 15:42:43 -0400

    Define/create User schema

--- models.js
+++ models.js
@@ -18,3 +18,11 @@ let movieSchema = mongoose.Schema({
     Featured: Boolean
 });
 
+let userSchema = mongoose.Schema({
+    Username: {type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
+    Birthday: Date,
+    FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+});
+
