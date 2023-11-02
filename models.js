6af2f574738c5ebcb27b70a8a9a27ddaffdab096
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2023-11-02 15:43:45 -0400

    Export models to make accessible elsewhere

--- models.js
+++ models.js
@@ -29,3 +29,5 @@ let userSchema = mongoose.Schema({
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
 
+module.exports.Movie = Movie;
+module.exports.User = User;
