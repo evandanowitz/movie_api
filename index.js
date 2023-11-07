d7209ce24f5e41415fd6ea5e2456ce1102250e26
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2023-11-07 17:44:34 -0500

    Delete old manually created array of movies

--- index.js
+++ index.js
@@ -20,58 +20,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-let topMovies = [
-    {
-    title: 'The Shawshank Redemption',
-    director: 'Frank Darabont',
-    year: '1994'
-    },
-    {
-    title: 'The Godfather',
-    author: 'Francis Ford Coppola',
-    year: '1972'
-    },
-    {
-    title: 'The Dark Knight',
-    author: 'Christopher Nolan',
-    year: '2008'
-    },
-    {
-    title:'The Godfather: Part II',
-    author: 'Francis Ford Coppola',
-    year: '1974'
-    },
-    {
-    title: '12 Angry Men',
-    author: 'Sidney Lumet',
-    year: '1957'
-    },
-    {
-    title: 'Schindler\'s List',
-    author: 'Steven Spielberg',
-    year: '1993'
-    },
-    {
-    title: 'The Lord of the Rings: The Return of the King',
-    author: 'Peter Jackson',
-    year: '2003'
-    },
-    {
-    title: 'Pulp Fiction',
-    author: 'Quentin Tarantino',
-    year: '1994'
-    },
-    {
-    title: 'The Lord of the Rings: The Fellowship of the Ring',
-    author: 'Peter Jackson',
-    year: '2001'
-    },
-    {
-    title: 'The Good, the Bad and the Ugly',
-    author: 'Sergio Leone',
-    year: '1966'
-    },
-];
 
 app.get('/', (req, res) => {
     res.send('Welcome to myFlix!');
