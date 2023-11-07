63ae4bb9ad4afc29c945946f7031280e199a40db
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2023-11-07 18:51:14 -0500

    Initialize Passport and Authentication Modules

---
+++ passport.js
@@ -0,0 +1,9 @@
+const passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    Models = require('./models.js'),
+    passportJWT = require('passport-jwt');
+
+let Users = Models.User,
+    JWTStrategy = passportJWT.Strategy,
+    ExtractJWT = passportJWT.ExtractJwt;
+
