850334c3f32deccc63dc935786a84bb831ba024a
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2023-11-13 11:53:43 -0500

    Implement CORS middleware
    Configure CORS middleware in Express
    Customize origin validation logic to allow specified origins and reject others

--- index.js
+++ index.js
@@ -20,6 +20,17 @@ const cors = require('cors');
 
 const { check, validationResult } = require('express-validator');
 
+app.use(cors({
+    origin: (origin, callback) => {
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn't found on the list of allowed origins
+            let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+            return callback(new Error(message ), false);
+        }
+        return callback(null, true);
+    }
+}));
+
 let auth = require('./auth')(app); // Imports 'auth.js' file into project
 
 const passport = require('passport'); // Requires Passport module
