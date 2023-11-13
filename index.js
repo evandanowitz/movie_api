2fed7a844b3d62bf5ee03e8a1c21baff91d6480e
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2023-11-13 11:59:15 -0500

    Add input validation and hash password function to "/users" endpoint

--- index.js
+++ index.js
@@ -94,31 +94,42 @@ app.get('/directors/:name', passport.authenticate('jwt', { session: false }), as
         });
 });
 
-app.post('/users', async (req, res) => { // POST a new user
-    await Users.findOne({ Username: req.body.Username })
-        .then((user) => {
-            if(user) {
-                return res.status(400).send(req.body.Username + ' already exists');
-            } else {
-                Users
-                    .create({
-                        ID: req.body.Id, // Integer
-                        Username: req.body.Username, // String
-                        Password: req.body.Password, // String
-                        Email: req.body.Email, // String
-                        Birthday: req.body.Birthday // Date
-                    })
-                    .then((user) => {res.status(201).json(user)})
-                .catch((error) => {
-                    console.error(error);
-                    res.status(500).send('Error: ' + error);
-                })
-            }
-        })
-        .catch((error) => {
+app.post('/users', 
+  [
+    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ], async (req, res) => { // POST a new user
+  let errors = validationResult(req); // Check the validation object for errors
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  let hashedPassword = Users.hashPassword(req.body.Password); // Hash any password entered by user when registering before storing it in the MongoDB database
+  await Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
+    .then((user) => {
+      if(user) { // If the user is found, send a response that it already exists
+        return res.status(400).send(req.body.Username + ' already exists');
+      } else {
+        Users
+          .create({
+            ID: req.body.Id, // Integer
+            Username: req.body.Username, // String
+            Password: req.body.Password, // String
+            Email: req.body.Email, // String
+            Birthday: req.body.Birthday // Date
+          })
+          .then((user) => {res.status(201).json(user)})
+          .catch((error) => {
             console.error(error);
             res.status(500).send('Error: ' + error);
-        });
+          });
+        }
+      })
+  .catch((error) => {
+    console.error(error);
+      res.status(500).send('Error: ' + error);
+  });
 });
 
 app.get('/users', passport.authenticate('jwt', { session: false }), async (req, res) => { // GET all users
